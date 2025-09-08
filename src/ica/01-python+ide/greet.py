9517106cf887d66a1e066557cff82a24c521082a
Ramirisa <iramirez@macalester.edu>
2025-09-08 09:30:12 -0500

    greet.commit

--- src/ica/01-python+ide/greet.py
+++ src/ica/01-python+ide/greet.py
@@ -1,5 +1,5 @@
 kind_person='Aunt Judi'
-'''Dear kind_person,
+print('''Dear kind_person,
 Thank you for your graduation present. I can't wait to use it. I
 I hope you are enjoying your summer. You're the best, kind_person!
-Love, Mittens'''
+Love, Mittens''')
