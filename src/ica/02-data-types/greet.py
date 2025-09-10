5230746022e8bd77301ce3f360ef67259067dd5e
Ramirisa <iramirez@macalester.edu>
2025-09-09 20:06:37 -0500

    triangle.py commit

--- src/ica/02-data-types/greet.py
+++ src/ica/02-data-types/greet.py
@@ -1,5 +1,5 @@
 kind_person='Aunt Judi'
-print('''Dear kind_person,
+print('''Dear Aunt Judi,
 Thank you for your graduation present. I can't wait to use it. I
-I hope you are enjoying your summer. You're the best, kind_person!
+I hope you are enjoying your summer. You're the best, Aunt Judi!
 Love, Mittens''')
