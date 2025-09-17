f70eb5dde5e8019ea370ee7758337594fa7c0fdb
Ramirisa <iramirez@macalester.edu>
2025-09-17 09:35:37 -0500

    Na

--- src/ica/02-data-types/greet.py
+++ src/ica/02-data-types/greet.py
@@ -1,5 +1,7 @@
 kind_person='Aunt Judi'
-print('''Dear Aunt Judi,
+print('''Dear kind_person,
 Thank you for your graduation present. I can't wait to use it. I
-I hope you are enjoying your summer. You're the best, Aunt Judi!
+I hope you are enjoying your summer. You're the best, kind_person!
 Love, Mittens''')
+print(kind_person)
+print(str)
