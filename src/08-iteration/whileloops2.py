cd300ff1dc9b8a716354e576c7a619e6c34508fd
Ramirisa <iramirez@macalester.edu>
2025-10-02 19:33:36 -0500

    whileloops2 commit

---
+++ src/08-iteration/whileloops2.py
@@ -0,0 +1,8 @@
+def add_user_nums():
+    sum_of_nums = 0
+    user_num = int(input("Enter a number (0 to quit): "))
+    while user_num != 0:
+        sum_of_nums += user_num
+        user_num = int(input("Enter a number (0 to quit): "))
+
+    return sum_of_nums
