e23c1308f07d9287f92cad200b3bb2e69df87a9a
Ramirisa <iramirez@macalester.edu>
2025-10-02 19:57:16 -0500

    whileloops3 commit

---
+++ src/08-iteration/whileloops3.py
@@ -0,0 +1,7 @@
+def square_user_nums2():
+  while True:
+      user_inp = input("Enter the next number (negative to quit): ")
+      user_num = int(user_inp)
+      if user_num < 0:
+        break
+        print(user_num, "squared is", user_num ** 2)
