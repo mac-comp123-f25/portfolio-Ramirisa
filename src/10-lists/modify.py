cb44e1633b81838a580db4fc801dfba23eccc699
Ramirisa <iramirez@macalester.edu>
2025-10-06 20:52:43 -0500

    modify commit

---
+++ src/10-lists/modify.py
@@ -0,0 +1,6 @@
+def change_start(value, lst):
+    """Change the first element (index 0) of the list to the given value."""
+    lst[0] = value
+t_list = ['a', 'b', 'c', 'd', 'e', 'f']
+change_start('gg', t_list)
+print(t_list)
